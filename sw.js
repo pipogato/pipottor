4bc0c51ba2d264e7e2aa8183aa6e215b9f9b3d0f
pipogato <pipogato248@hotmail.com>
2018-11-26 14:37:06 -0500

    Modificacion pata ios

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //Importaciones
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static_v2';
+const STATIC_CACHE = 'static_v3';
 const DYNAMIC_CACHE = 'dynamic_v1';
 const INMUTABLE_CACHE = 'inmutable_v1';
 
